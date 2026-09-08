39b578109ade38d7ebfbef1ab1c257eed0be8d33
SirCesarium <239650612+SirCesarium@users.noreply.github.com>
2026-09-08 13:50:24 -0400

    fix: add data/storage files

---
+++ src/main/java/net/sircesarium/cartography/data/storage/ChunkData.java
@@ -0,0 +1,80 @@
+package net.sircesarium.cartography.data.storage;
+
+import java.nio.file.Path;
+
+import lombok.Getter;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.server.level.ServerLevel;
+import net.sircesarium.cartography.data.layers.DecorationsLayer;
+import net.sircesarium.cartography.data.layers.TerrainLayer;
+import net.sircesarium.cartography.data.layers.VegetationLayer;
+import net.sircesarium.cartography.data.layers.WaterLayer;
+import net.sircesarium.cartography.util.NBTData;
+
+@Getter
+public class ChunkData extends NBTData {
+    private final int chunkX;
+    private final int chunkZ;
+    @Getter
+    private boolean dirty;
+
+    private TerrainLayer terrain;
+    private WaterLayer water;
+    private VegetationLayer vegetation;
+    private DecorationsLayer decorations;
+
+    public ChunkData(ServerLevel level, int chunkX, int chunkZ) {
+        super(level, "chunks/c." + chunkX + "." + chunkZ + ".dat");
+        this.chunkX = chunkX;
+        this.chunkZ = chunkZ;
+        this.dirty = false;
+        this.terrain = new TerrainLayer();
+        this.water = new WaterLayer();
+        this.vegetation = new VegetationLayer();
+        this.decorations = new DecorationsLayer();
+    }
+
+    public ChunkData(Path basePath, String fileName, int chunkX, int chunkZ) {
+        super(basePath, fileName);
+        this.chunkX = chunkX;
+        this.chunkZ = chunkZ;
+        this.dirty = false;
+        this.terrain = new TerrainLayer();
+        this.water = new WaterLayer();
+        this.vegetation = new VegetationLayer();
+        this.decorations = new DecorationsLayer();
+    }
+
+    public void markDirty() {
+        this.dirty = true;
+    }
+
+    public void saveIfDirty() {
+        if (dirty) {
+            save();
+            dirty = false;
+        }
+    }
+
+    public void loadFromNBT(CompoundTag tag) {
+        fromNBT(tag);
+    }
+
+    @Override
+    public CompoundTag toNBT() {
+        CompoundTag tag = new CompoundTag();
+        tag.put("terrain", terrain.toNBT());
+        tag.put("water", water.toNBT());
+        tag.put("vegetation", vegetation.toNBT());
+        tag.put("decorations", decorations.toNBT());
+        return tag;
+    }
+
+    @Override
+    protected void fromNBT(CompoundTag tag) {
+        this.terrain = TerrainLayer.load(tag.getCompound("terrain"));
+        this.water = WaterLayer.load(tag.getCompound("water"));
+        this.vegetation = VegetationLayer.load(tag.getCompound("vegetation"));
+        this.decorations = DecorationsLayer.load(tag.getCompound("decorations"));
+    }
+}
